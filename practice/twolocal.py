a5ac8f5fefefeda78d25065e9f77a0d861f46d4a
Yosef Shammout <YosefShammout123@gmail.com>
2025-09-21 14:47:42 -0400

    explanation of twolocal

--- practice/twolocal.py
+++ practice/twolocal.py
@@ -3,3 +3,6 @@ from qiskit.circuit.library import TwoLocal
 two_local = TwoLocal(3, rotation_blocks = "rx", entanglement_blocks = "cz") # 3 qubits
 bound = two_local.assign_parameters([0] * 12) # set all sliders to 0
 print(bound.decompose().draw('text'))
+
+#rx rotates each qubit like a spinning coin, and CZ link pairs like gluing coins together.
+#Parameters(sliders) let you tune the circuit for optimization
