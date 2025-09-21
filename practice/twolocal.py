0485eb96c5a7d29583272d85e0f0f0e974e8a994
Yosef Shammout <YosefShammout123@gmail.com>
2025-09-21 18:48:27 -0400

    QAOAA

--- practice/twolocal.py
+++ practice/twolocal.py
@@ -1,8 +1,10 @@
 from qiskit.circuit.library import TwoLocal
-
+from qiskit.quantum_info import Statevector
 two_local = TwoLocal(3, rotation_blocks = "rx", entanglement_blocks = "cz") # 3 qubits
 bound = two_local.assign_parameters([0] * 12) # set all sliders to 0
+state = Statevector.from_instruction(bound)
+print(state.probabilities())
 print(bound.decompose().draw('text'))
-
 #rx rotates each qubit like a spinning coin, and CZ link pairs like gluing coins together.
 #Parameters(sliders) let you tune the circuit for optimization
+
