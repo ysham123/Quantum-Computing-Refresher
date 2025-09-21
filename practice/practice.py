142dbd5de5b9ed4991410c6aaef0060a0bf471cd
Yosef Shammout <YosefShammout123@gmail.com>
2025-09-21 14:28:38 -0400

    practicing

---
+++ practice/practice.py
@@ -0,0 +1,11 @@
+from qiskit import QuantumCircuit
+
+qc = QuantumCircuit(1)
+qc.h(0)
+print(qc.draw('text'))
+
+from qiskit.quantum_info import Statevector
+qc = QuantumCircuit(1)
+qc.h(0)
+
+print(Statevector.from_instruction(qc).probabilities())
