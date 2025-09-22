da20b62fda46a359c0b49c43fbc371114f3e05a2
Yosef Shammout <YosefShammout123@gmail.com>
2025-09-21 20:37:41 -0400

    testing

--- practice/practice.py
+++ practice/practice.py
@@ -1,3 +1,4 @@
+from sre_parse import State
 from qiskit import QuantumCircuit
 
 qc = QuantumCircuit(1)
@@ -8,5 +9,22 @@ from qiskit.quantum_info import Statevector
 qc = QuantumCircuit(1)
 qc.h(0)
 
+print(Statevector.from_instruction(qc).probabilities())
+
+
+from qiskit import QuantumCircuit
+from qiskit.quantum_info import Statevector
+
+qc = QuantumCircuit(1)
+qc.h(0)
+
+print(Statevector.from_instruction(qc).probabilities())
+
+#quantum gate
+
+from qiskit import QuantumCircuit
+qc = QuantumCircuit(2)
+qc.x(0)
+qc.cx(0,1)
 print(Statevector.from_instruction(qc).probabilities())
 
