43b4308040cd17b85ac883004324438fc65c7f42
Yosef Shammout <YosefShammout123@gmail.com>
2025-09-22 13:55:29 -0400

    more practice

--- practice/practice.py
+++ practice/practice.py
@@ -28,3 +28,17 @@ qc.x(0)
 qc.cx(0,1)
 print(Statevector.from_instruction(qc).probabilities())
 
+#superposition and entanglement
+from qiskit import QuantumCircuit
+from qiskit.quantum_info import Statevector
+qc = QuantumCircuit(2)
+qc.h(0)
+qc.cx(0,1)
+print(Statevector.from_instruction(qc).probabilities())
+print(qc.draw('text'))
+
+from qiskit.circuit.library import TwoLocal
+two_local = TwoLocal(3, 'rx', 'cz')
+bound = two_local.assign_parameters([0.1] * 12)
+print(bound.decompose().draw('text'))
+
